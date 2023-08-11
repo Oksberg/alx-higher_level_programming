a9215b102ec12aa6143f4f2a6a07d00a4218f31e
Okyere Adubofuor <oksberg89@gmail.com>
2023-08-11 06:48:45 -0700

    Print last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+output_string = "Last digit of {} is {} {}"
+status = ("and is greater than 5", "and is 0", "and is less than 6 and not 0")
 last_digit = number % 10
+if last_digit < 0:
+    last_digit = -last_digit
 if last_digit > 5:
-    print("{0} {1} {2} {3} {status[0]}".format("Last digit of", number, "is", last_digit, status = ("and is greater than 5", "and is 0", "and is less than 6 and not 0")))
+    print(output_string.format(number, last_digit, status[0]))
 elif last_digit == 0:
-    print("{0} {1} {2} {3} {status[1]}".format("Last digit of", number, "is", last_digit, status = ("and is greater than 5", "and is 0", "and is less than 6 and not 0")))
-elif last_digit > 6 and last_digit != 0:
-    print("{0} {1} {2} {3} {status[2]}".format("Last digit of", number, "is", last_digit, status = ("and is greater than 5", "and is 0", "and is less than 6 and not 0")))
+    print(output_string.format(number, last_digit, status[1]))
+elif last_digit < 6 and last_digit != 0:
+    print(output_string.format(number, last_digit, status[2]))
