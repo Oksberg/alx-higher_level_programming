c4e02d06939bcbd077e533fb698f9a87304ff61a
Okyere Adubofuor <oksberg89@gmail.com>
2023-08-11 19:40:54 -0700

    Commandline arguments in Python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import sys
-
-num_of_args = len(sys.argv)
-if num_of_args == 1:
-    if __name__ == "__main__":
+if __name__ == "__main__":
+    num_of_args = len(sys.argv)
+    if num_of_args == 1:
         print("0 arguments.")
-else:
-    print("{} arguments:".format(num_of_args - 1))
-    for i in range(num_of_args - 1):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    elif num_of_args == 2:
+        print("{} argument:".format(num_of_args - 1))
+        print("{}: {}".format(num_of_args -  1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(num_of_args - 1))
+        for i in range(num_of_args - 1):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
