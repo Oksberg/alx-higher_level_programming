5c451d1d1a61b36c7b9301daa88c2e6b98f13155
Okyere Adubofuor <oksberg89@gmail.com>
2023-08-28 22:09:16 -0700

    List division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = my_list_1[i] / my_list_2[i]
         except ValueError:
             result = 0
-        except  TypeError:
+        except TypeError:
             print("wrong type")
             result = 0
         except ZeroDivisionError:
