2c3d075214c11126782e77ae2693df44786801ca
Okyere Adubofuor <oksberg89@gmail.com>
2023-08-30 20:52:03 -0700

    Create an empty object

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bins/python3
+
+class Square:
+    """This is an empty class without any attribute or methods.
+    It is not initialiazed and its Square object has not defined size"""
+    pass
