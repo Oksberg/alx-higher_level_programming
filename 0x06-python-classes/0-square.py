c4adae4b840c4dfcb8be16e08d14657a0428d00c
Okyere Adubofuor <oksberg89@gmail.com>
2023-08-30 22:34:48 -0700

    Adding Readme

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,9 +2,5 @@
 
 """Create a class called Square."""
 class Square:
-    """A class that represents a square.
-
-    This is an empty class without any attribute or methods.
-    It is not initialiazed and its Square object has not defined size.
-    """
+    """A class that represents a square."""
     pass
