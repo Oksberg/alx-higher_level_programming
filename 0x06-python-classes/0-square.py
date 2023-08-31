a9090a99b0171496308cb9945c0cd68057d7da9d
Okyere Adubofuor <oksberg89@gmail.com>
2023-08-30 22:23:52 -0700

    Adding Readme

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,12 @@
 #!/usr/bins/python3
 
 class Square:
-    """This is an empty class without any attribute or methods.
-    It is not initialiazed and its Square object has not defined size"""
+    """A class object called Square.
+
+    This is an empty class without any attribute or methods.
+    It is not initialiazed and its Square object has not defined size.
+
+    Attributes:
+        size (int): The length of the square's sides.
+        """
     pass
