23a058062992c37d3ee5ddf11cf0fc852657fd49
Okyere Adubofuor <oksberg89@gmail.com>
2023-09-03 10:25:33 -0700

    Adding position attribute

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -75,8 +75,9 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or len(value) != 2 or
+        if (not isinstance(value, tuple) or
+            len(value) != 2 or
             not all(isinstance(i, int) for i in value) or
-            not all(i >= 0 for i in value)):
-          raise TypeError("position must be a tuple of 2 positive integers")
+                not all(i >= 0 for i in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
