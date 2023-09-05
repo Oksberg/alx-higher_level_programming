a0b4725f8ce9a831a70d632215ee198114789c8d
Okyere Adubofuor <oksberg89@gmail.com>
2023-09-04 17:12:30 -0700

    An empty class Rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+
+
 class Rectangle:
     """This is an empty class without any attribute or method
+
+    Attributes and methods will be added in the subsequent tasks
     """
     pass
