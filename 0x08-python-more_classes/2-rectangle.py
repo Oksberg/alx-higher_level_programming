8b6c48d8b100e79e8574f6c86b73659cbfbcd2c5
Okyere Adubofuor <oksberg89@gmail.com>
2023-09-04 19:09:59 -0700

    Gets area and perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -59,4 +59,6 @@ class Rectangle:
 
     def perimeter(self):
         """This public method returns the perimeter of the object"""
+        if self.__width == 0 and self.__height == 0:
+            return 0
         return 2 * self.__width + 2 * self.__height
