cfa7d4a5c7ee29fd7060fd8ee067ab94afc35783
Okyere Adubofuor <oksberg89@gmail.com>
2023-09-04 18:52:21 -0700

    Added width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,8 +16,8 @@ class Rectangle:
             width: The rectangle must have a width but defaults to 0
             height: Value must be specified but defaults to 0.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -50,5 +50,5 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("heigth must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
