26677c9b7b07e069ba815272cd1fa3006656cc1a
Okyere Adubofuor <oksberg89@gmail.com>
2023-09-11 21:44:02 -0700

    Added docstring

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""A function """
+
 def lookup(obj):
+    """The function definition
+    This function returns the list of all attributes and
+    methods available to the obj object.
+    """
+
     return dir(obj)
