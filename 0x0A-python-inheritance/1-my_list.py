19eb110075a88b65a80223e583034fa93cdf1955
Okyere Adubofuor <oksberg89@gmail.com>
2023-09-13 23:26:45 -0700

    Print sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class MyList(list):
+    """Defines a function that sorts data."""
+
+
+    def print_sorted(self):
+        """This function returns a list sorted inascending order."""
+        
+        return self.sort()
