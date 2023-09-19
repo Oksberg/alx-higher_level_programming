6c76dcff368b0a3f6978358b43d9e42e42e13f3d
Okyere Adubofuor <oksberg89@gmail.com>
2023-09-19 03:19:10 -0700

    Rectangle corrected

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Defines a class Rectangle."""
-
-
 from models.base import Base
 
 
@@ -14,14 +12,23 @@ class Rectangle(Base):
         Args:
             width (int): The width of the Rectangle, it is required.
             height (int): The height, defaults, it is required.
-            x (int): An attribute of the Rectangle, defaults to 0
-            y (int): An attribute of the Rectangle, defaults to 0
+            x (int): The x-coordinate of the Rectangle, defaults to 0.
+            y (int): y-coordinate of the Rectangle, defaults to 0.
+            id (int): The identity of the Rectangle.
+
+        Raise:
+            TypeError: If any of the inputs is not of type int with
+            message '<name of the attribute> must be an integer'.
+            ValueError: If width or height is <= 0 with message
+            '<name of the attribute> must be > 0'.
+            ValueError: If x or y is < 0 with messsage
+            '<name of the attribute> must be >= 0'.
         """
 
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
@@ -37,7 +44,7 @@ class Rectangle(Base):
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -54,7 +61,7 @@ class Rectangle(Base):
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
@@ -71,7 +78,7 @@ class Rectangle(Base):
     def x(self, value):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -88,6 +95,6 @@ class Rectangle(Base):
     def y(self, value):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
