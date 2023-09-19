91ab163487236b67734aa0013f6ffa49ac733341
Okyere Adubofuor <oksberg89@gmail.com>
2023-09-19 04:15:29 -0700

    Added a new __str__ method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -109,3 +109,7 @@ class Rectangle(Base):
             for j in range(self.width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """Returns a string representation of the attributes of the Rectangle"""
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
