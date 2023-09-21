8d79ae0d5146783eed399f4ee3e0902343a4115e
Okyere Adubofuor <oksberg89@gmail.com>
2023-09-21 16:02:03 -0700

    implementing args

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -123,3 +123,18 @@ class Rectangle(Base):
         width = self.width
         height = self.height
         return f"[Rectangle] ({id}) {x}/{y} - {width}/{height}"
+
+    def update(self, *args, **kwargs):
+        """Assigns arguments and keyworg arguments
+        to the attributes."""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
