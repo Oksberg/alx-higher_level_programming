923a8830713d9fa2d3680bf160f2abaa3f75463d
Okyere Adubofuor <oksberg89@gmail.com>
2023-09-23 05:15:50 -0700

    Square class inherits from Rectangle class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,3 +16,28 @@ class Square(Rectangle):
         """
 
         super().__init__(size, size, x, y, id)
+
+    def validate_width(self, size):
+        """Calls inherited width getter and setter."""
+        self.__width = size
+
+    def validate_width(self, size):
+        """Calls inherited height getter and setter."""
+        self.__height = size
+
+    def validate_x(self, value):
+        """Calls inherited x getter and setter."""
+        self.__x = x
+
+    def validate_x(self, value):
+        """Calls inherited x getter and setter."""
+        self.__y = y
+
+    def __str__(self):
+        """Returns a string rep of the Square"""
+        id = self.id
+        x = self.x
+        y = self.y
+        size = self.width
+        size = self.height
+        return f"[Square] ({id}) {x}/{y} - {size}"
