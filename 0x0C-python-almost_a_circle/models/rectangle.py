10469a3d1a3798dbade2593b7b0b56820f0275fd
Okyere Adubofuor <oksberg89@gmail.com>
2023-09-23 23:07:22 -0700

    attribute to dictionary

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -145,4 +145,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """Returns the dictionary representation of a Rectangle."""
-        return vars(self)
+        return {'id': self.id, 'width': self.width,
+            'height': self.height, 'x': self.x, 'y': self.y}
