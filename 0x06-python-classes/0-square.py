6ec27f803f09b48792110f763ecbe85c533c6462
Okyere Adubofuor <oksberg89@gmail.com>
2023-10-25 09:25:30 +0000

    Delete 0x06-python-classes directory
    Deleted in order to fulfill an ALX requirement after deferring.

--- 0x06-python-classes/0-square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Create a class Square."""
-
-
-class Square:
-    """This square object is a real representation of a real world square.
-    """
-    pass
--- 0x06-python-classes/README.md
+++
@@ -1,7 +0,0 @@
-Task 0
-------
-This script writes a class called "Square" that defines a square.
-No module is imported.
-
-Task 1
-------
