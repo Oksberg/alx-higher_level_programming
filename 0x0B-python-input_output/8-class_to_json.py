60ddd1f03aaf293e34619cb4511f6d8f3f1b4f8c
Okyere Adubofuor <oksberg89@gmail.com>
2023-10-25 09:27:26 +0000

    Delete 0x0B-python-input_output directory
    Deleted in order to fulfill an ALX requirement after deferring.

--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""This function makes a Python class-to-JSON."""
-
-
-def class_to_json(obj):
-    """This function returns the dictionary representation of a
-    simple data structure."""
-
-    return obj.__dict__
