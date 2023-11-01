3d658007b4bb0ec583414e5107109e811de56d9c
Okyere <oksberg89@gmail.com>
2023-10-31 22:58:50 -0700

    setting attribute

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-class LockedClass(object):
-    __slots__ = "first_name"
+class LockedClass:
+    def __setattr__(self, name, value):
+        if name != "first_name":
+            raise AttributeError(f"LockedClass' object has no attribute '{name}'")
+        super().__setattr__(name, value)
