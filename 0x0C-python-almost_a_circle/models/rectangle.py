d228612310c4c21a660f1858a847a857a7c70478
Okyere <oksberg89@gmail.com>
2023-11-14 19:15:06 -0800

    Update str rep

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -125,16 +125,16 @@ class Rectangle(Base):
         """Assigns arguments and keyword arguments to
         the attributes."""
 
-        if len(args) >= 1:
+        if args:
             self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = 3
-        if len(args) >= 5:
-            self.y = 4
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
 
         for key, value in kwargs.items():
             if hasattr(self, key):
