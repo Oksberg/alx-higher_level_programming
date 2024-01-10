583620e493c27803ac427e35adb742013139e486
Okyere Adubofuor <oksberg89@gmail.com>
2024-01-09 21:57:46 -0800

    Introduction to JavaScript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let str = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const str = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 
 while (i < str.length) {
