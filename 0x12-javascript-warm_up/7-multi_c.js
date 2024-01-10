4d0b62d8b245adcc02ef517c40285867d7ba23af
Okyere Adubofuor <oksberg89@gmail.com>
2024-01-09 22:12:15 -0800

    Introduction to JavaScript

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 const { argv } = require('node:process');
 const num = Number(argv[2]);
 
-if (!isNaN(num)) {
-  for (let i = 0; i < num; i++) {
-    console.log('C is fun');
-  }
-} else {
+if (isNaN(num)) {
   console.log('Missing number of occurrences');
-}
+} else {
+    for (let i = 0; i < num; i++) {
+      console.log('C is fun');
+    }
+  }
