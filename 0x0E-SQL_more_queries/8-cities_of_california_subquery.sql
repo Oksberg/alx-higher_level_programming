40c8b9411ad76243bdfaed569fe71e38f0055a64
Okyere Adubofuor <oksberg89@gmail.com>
2024-03-20 18:29:49 -0700

    Selected id and name

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- Lists all the cities of California found in hbtn_0d_usa
 -- Results must be sorted in ascending order by cities.id
-SELECT * FROM cities
+SELECT id, name FROM cities
 WHERE state_id=(SELECT id FROM states WHERE name='California')
 ORDER BY id ASC;
