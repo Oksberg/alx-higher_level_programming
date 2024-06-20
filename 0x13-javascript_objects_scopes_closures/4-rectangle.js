a35dc114ab8d4010c6d88be0a52ec2046367523c
Okyere Adubofuor <oksberg89@gmail.com>
2024-06-19 21:29:14 -0700

    OOP in JS

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width > 0 && this.height > 0) {
+      for (let i = 0; i < this.height; i++) {
+            console.log('X'.repeat(this.width));
+      }
+    }
+  }
+
+  rotate () {
+    if (this.width > 0 && this.height > 0) {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,3 @@
+# 0x13-javascript_objects_scopes_closures
+This project introduces OOP in JavaScript.
+It deals with the basics initially.
